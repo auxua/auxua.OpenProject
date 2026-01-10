65f405c651fd8c011b1bb65613b3336bcc5ba03b
Arno Schmetz <arno.schmetz@rwth-aachen.de>
2026-01-10 21:29:59 +0100

    adding Settings template

---
+++ OpenProjectClient/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenProjectClient
+{
+    internal class Settings
+    {
+        public const string ApiBaseUrl = "https://openproject.example.com/api/v3/"; 
+        public const string ApiKey = "your_api_key_here";
+    }
+}
